2c0bf732955db77b30145b5c45b8053e1edca3ff
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2022-12-02 14:12:54 +0800

    style(footer): add cols

--- components/DefaultFooter.tsx
+++ components/DefaultFooter.tsx
@@ -1,37 +1,81 @@
-import {Footer} from "@govtechsg/sgds-react";
-import {BRAND_NAME, ORGANISATION_NAME} from "../lib/config";
+import { Footer } from "@govtechsg/sgds-react";
+import { ORGANISATION_NAME } from "../lib/config";
 
 export default function DefaultFooter() {
   return (
-    <Footer>
-      <Footer.Top>
-        <h1>{ORGANISATION_NAME}</h1>
-        <Footer.Top.Header>
-          {ORGANISATION_NAME} serves citizens and business by being a constant partner in government to support both parties needs.
-        </Footer.Top.Header>
-        <Footer.Top.ContactLinks>
-          <a href="" target="_blank">
-            Contact
-          </a>
-          <a href="" target="_blank">
-            Feedback
-          </a>
-        </Footer.Top.ContactLinks>
-      </Footer.Top>
-      <Footer.Bottom>
-        <Footer.Bottom.Links>
-          <a href="" target="_blank">
-            Report Vulnerability
-          </a>
-          <a href="">Privacy Statement</a>
-          <a href="">Terms of use</a>
-        </Footer.Bottom.Links>
-        <Footer.Bottom.Copyrights className="text-end">
-          © 2022 Government of Singapore
-          <br/>
-          Last Updated 08 Feb 2022
-        </Footer.Bottom.Copyrights>
-      </Footer.Bottom>
-    </Footer>
-  )
+      <Footer>
+        <Footer.Top>
+          <h1>{ORGANISATION_NAME}</h1>
+          <Footer.Top.Header>
+          {ORGANISATION_NAME} serves citizens and business by being a constant partner in
+            government to support both parties needs
+          </Footer.Top.Header>
+          <Footer.Top.ItemGroup>
+            <Footer.Top.Item itemTitle='Column 1'>
+              <a href=''>About Us</a>
+              <a href=''>This is a super long link</a>
+              <a href=''>Test</a>
+              <a href=''>Test</a>
+            </Footer.Top.Item>
+            <Footer.Top.Item itemTitle='Column 2'>
+              <a href=''>About Us</a>
+              <a href=''>This is a super long link</a>
+              <a href=''>Test</a>
+              <a href=''>Test</a>
+            </Footer.Top.Item>
+            <Footer.Top.Item itemTitle='Column 3'>
+              <a href=''>About Us</a>
+              <a href=''>This is a super long link</a>
+              <a href=''>Test</a>
+              <a href=''>Test</a>
+            </Footer.Top.Item>
+            <Footer.Top.Item itemTitle='Column 4'>
+              <a href=''>About Us</a>
+              <a href=''>This is a super long link</a>
+              <a href=''>Test</a>
+              <a href=''>Test</a>
+            </Footer.Top.Item>
+            <Footer.Top.Item itemTitle='Column 5'>
+              <a href=''>About Us</a>
+              <a href=''>This is a super long link</a>
+              <a href=''>Test</a>
+              <a href=''>Test</a>
+            </Footer.Top.Item>
+            <Footer.Top.Item itemTitle='Column 6'>
+              <a href=''>About Us</a>
+              <a href=''>This is a super long link</a>
+              <a href=''>Test</a>
+              <a href=''>Test</a>
+            </Footer.Top.Item>
+          </Footer.Top.ItemGroup>
+          <Footer.Top.ContactLinks>
+            <a href=''>Contact</a>
+            <a href=''>Feedback</a>
+            <a
+              href='https://www.reach.gov.sg/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Reach.gov.sg
+            </a>
+          </Footer.Top.ContactLinks>
+        </Footer.Top>
+        <Footer.Bottom>
+          <Footer.Bottom.Links>
+            <a
+              href='https://tech.gov.sg/report_vulnerability'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Report Vulnerability
+            </a>
+            <a href=''>Privacy</a>
+            <a href=''>Terms of use</a>
+          </Footer.Bottom.Links>
+          <Footer.Bottom.Copyrights>
+            © 2022 Government of Singapore. Last Updated 08 Feb 2022
+          </Footer.Bottom.Copyrights>
+        </Footer.Bottom>
+      </Footer>
+  );
 }
