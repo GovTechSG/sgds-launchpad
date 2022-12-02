f848e18f8977f5b6cb1a920837697a6da588c002
hafizi <61571184+hafiz-ism@users.noreply.github.com>
2022-12-02 14:44:25 +0800

    feat(landingpage): added styling and mobile responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,112 +1,225 @@
-import type {NextPage} from 'next'
-import Head from 'next/head'
-import {Button, Col, Container, Row} from "@govtechsg/sgds-react";
-import {BRAND_NAME, ORGANISATION_NAME} from "../lib/config";
-import {LandingPageLayout} from "../components/layouts/LandingPageLayout";
-import withLink from "../components/WithLink";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Button, Col, Container, Row, Card } from "@govtechsg/sgds-react";
+import { BRAND_NAME, ORGANISATION_NAME } from "../lib/config";
+import { LandingPageLayout } from "../components/layouts/LandingPageLayout";
 import Image from "next/image";
-import bg_people from "../public/bg_people.png";
-import construction from "../public/img_construction.png";
-import banner from "../public/homepage-banner.png";
-import {ArrowRight, CardChecklist, Compass, People, PersonCheck, QuestionCircle} from "react-bootstrap-icons";
-import {Stack} from "react-bootstrap";
+import herobanner from "../public/hero_banner.png";
+import block1pic from "../public/block1_pic.png";
+import block4pic from "../public/block4_pic.png";
+import leftwing from "../public/left-wing.png";
+import rightwing from "../public/right-wing.png";
+import image1 from "../public/image-1.png";
+import image2 from "../public/image-2.png";
+import image3 from "../public/image-3.png";
+import image4 from "../public/image-4.png";
+import { Stack } from "react-bootstrap";
 
 const Home: NextPage = () => {
-  const title = `${BRAND_NAME}`
+  const title = `${BRAND_NAME}`;
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={title}/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta name='description' content={title} />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <LandingPageLayout>
-        <div className="has-background-gradient">
-          <Container className="p-5">
-            <Row className="py-md-5">
+        <Container>
+          <Row className='py-5 d-flex align-items-center'>
+            <Col className='d-block d-md-none'>
+              <Image
+                alt='3 people discussing rendered design'
+                src={herobanner}
+              />
+            </Col>
+            <Col md={12} lg={6} className='px-1'>
+              <h3>
+                Header.<br></br>Header. Header.
+              </h3>
+              <p>
+                Additional intro copy to introduce the page and lead users into
+                the content.
+              </p>
+              <div className='d-grid d-md-block'>
+                <Button>Call-to-action</Button>
+              </div>
+            </Col>
+            <Col className='d-none d-md-block'>
+              <Image
+                alt='3 people discussing rendered design'
+                src={herobanner}
+              />
+            </Col>
+          </Row>
+        </Container>
+        <div className='d-block d-md-none bg-purple-200'>
+          <Container>
+            <Row className='py-md-5'>
               <Col>
-                <Stack className="text-center" gap={2}>
-                  <h1>{BRAND_NAME}</h1>
-                  <h5>Supporting our citizens and businesses for a better Singapore</h5>
-                  <Stack className="justify-content-center flex-column flex-md-row" gap={2}>
-                    {withLink(<Button variant={"outline-light"}><QuestionCircle/> Find out more</Button>, "/guides")}
-                    {withLink(<Button variant={"light"}>How to Collect <ArrowRight/></Button>, "/collection")}
-                  </Stack>
+                <Stack className='text-center'>
+                  <h1>TITLE.</h1>
+                  <p>
+                    Contrary to popular belief, Lorem Ipsum is not simply random
+                    text. It has roots in a piece of classical Latin literature
+                    from 45 BC, making it over 2000 years old. Richard
+                    McClintock, a Latin professor at Hampden-Sydney College in
+                    Virginia.
+                  </p>
                 </Stack>
               </Col>
             </Row>
           </Container>
         </div>
         <Container>
-          <Row className="py-5">
-            <Col className="p-4 d-flex align-content-end">
-              <Stack>
-                <Image layout="responsive" alt="two people discussing code and the rendered design" src={banner}/>
-              </Stack>
+          <Row className='py-5 d-flex align-items-center'>
+            <Col>
+              <Image alt='a person within a phone display' src={block1pic} />
             </Col>
-            <Col md={12} lg={8} className="p-4 d-flex align-items-center">
-              <Stack>
-                <h3>Citizens</h3>
-                <p>
-                  Doggo ipsum heckin long doggo long water shoob length boy dat tungg tho, borkdrive you are doing me
-                  the shock. blep doge puggorino. Doggorino big ol pupper floofs clouds he made many woofs mlem, floofs
-                  fluffer clouds big ol pupper, very jealous pupper bork lotsa pats wow such tempt.
-                </p>
-                <Stack className="flex-column flex-md-row flex-grow-0" gap={2}>
-                  {withLink(<Button variant={"outline-primary"}><PersonCheck/> Supporting Citizens</Button>, "/citizens")}
-                  {withLink(<Button variant={"primary"}>Collect Now <ArrowRight/></Button>, "/collection")}
-                </Stack>
-              </Stack>
-            </Col>
-          </Row>
-          <Row className="py-5 flex-row-reverse">
-            <Col className="p-4 d-flex align-content-end">
-              <Stack>
-                <Image layout="responsive" alt="three people working on a webpage under construction"
-                       src={construction}/>
-              </Stack>
-            </Col>
-            <Col md={12} lg={8} className="p-4 d-flex align-items-center">
-              <Stack className="align-items-end text-end">
-                <h3>Businesses</h3>
-                <p>
-                  Doggo ipsum heckin long doggo long water shoob length boy dat tungg tho, borkdrive you are doing me
-                  the shock. blep doge puggorino. Doggorino big ol pupper floofs clouds he made many woofs mlem, floofs
-                  fluffer clouds big ol pupper, very jealous pupper bork lotsa pats wow such tempt.
-                </p>
-                <Stack className="flex-column flex-md-row flex-grow-0 justify-content-end" gap={2}>
-                  {withLink(<Button variant={"outline-primary"}><People/> Supporting Businesses</Button>, "/businesses")}
-                  {withLink(<Button variant={"primary"}>Collect Now <ArrowRight/></Button>, "/collection")}
-                </Stack>
-              </Stack>
-            </Col>
-          </Row>
-          <Row className="py-5">
-            <Col className="p-4 d-flex align-content-end">
-              <Stack>
-                <Image layout="responsive" alt="two people discussing a dashboard" src={bg_people}/>
-              </Stack>
-            </Col>
-            <Col md={12} lg={8} className="p-4 d-flex align-items-center">
-              <Stack>
-                <h3>Guides from {ORGANISATION_NAME} </h3>
-                <p>
-                  Doggo ipsum heckin long doggo long water shoob length boy dat tungg tho, borkdrive you are doing me
-                  the shock. blep doge puggorino. Doggorino big ol pupper floofs clouds he made many woofs mlem, floofs
-                  fluffer clouds big ol pupper, very jealous pupper bork lotsa pats wow such tempt.
-                </p>
-                <Stack className="flex-column flex-md-row flex-grow-0" gap={2}>
-                  {withLink(<Button variant={"outline-primary"}><Compass/> More about {ORGANISATION_NAME}</Button>, "/faq")}
-                  {withLink(<Button variant={"primary"}>Guides and Case Studies <ArrowRight/></Button>, "/guides")}
-                </Stack>
-              </Stack>
+            <Col md={12} lg={6}>
+              <p className='position-relative'>
+                <span id='leftwing' className='d-none d-md-block'>
+                  <Image alt='left wing' src={leftwing} />
+                </span>
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece of classical Latin literature from
+                45 BC, making it over 2000 years old. Richard McClintock, a
+                Latin professor at Hampden-Sydney College in Virginia, looked up
+                one of the more obscure Latin words, consectetur, from a Lorem
+                Ipsum passage, and going through the cites of the word in
+                classical literature, discovered the undoubtable source.
+                <span id='rightwing' className='d-none d-md-block'>
+                  <Image alt='right wing' src={rightwing} />
+                </span>
+              </p>
             </Col>
           </Row>
         </Container>
+        <div className='d-none d-md-block bg-purple-200'>
+          <Container>
+            <Row className='py-md-5'>
+              <Col>
+                <Stack className='text-center'>
+                  <h1>TITLE.</h1>
+                  <p>
+                    Contrary to popular belief, Lorem Ipsum is not simply random
+                    text. It has roots in a piece of classical Latin literature
+                    from 45 BC, making it over 2000 years old. Richard
+                    McClintock, a Latin professor at Hampden-Sydney College in
+                    Virginia.
+                  </p>
+                </Stack>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <Container>
+          <Row className='py-md-5'>
+            <Col>
+              <h3 className='pb-3'>Content Block 3</h3>
+              <Row>
+                <Col md={3} xs={12} className='pb-3'>
+                  <Card>
+                    <Card.Img
+                      alt='img alternate text goes here'
+                      src={image1.src}
+                      variant='top'
+                    />
+                    <Card.Body>
+                      <Card.Title>Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md={3} xs={12} className='pb-3'>
+                  <Card>
+                    <Card.Img
+                      alt='img alternate text goes here'
+                      src={image2.src}
+                      variant='top'
+                    />
+                    <Card.Body>
+                      <Card.Title>Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md={3} xs={12} className='pb-3'>
+                  <Card>
+                    <Card.Img
+                      alt='img alternate text goes here'
+                      src={image3.src}
+                      variant='top'
+                    />
+                    <Card.Body>
+                      <Card.Title>Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md={3} xs={12} className='pb-3'>
+                  <Card>
+                    <Card.Img
+                      alt='img alternate text goes here'
+                      src={image4.src}
+                      variant='top'
+                    ></Card.Img>
+                    <Card.Body>
+                      <Card.Title>Title</Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+        <div className='bg-grey-100'>
+          <Container>
+            <Row className='py-5 d-flex align-items-center'>
+              <Col md={{ span: 6, offset: 2 }} className='d-block d-md-none'>
+                <Image alt='person watching screen' src={block4pic} />
+              </Col>
+              <Col md={4} className=''>
+                <Card className='bg-grey-600' text='white'>
+                  <Card.Body>
+                    <Card.Title>Content Block 4</Card.Title>
+                    <Card.Text>
+                      Contrary to popular belief, Lorem Ipsum is not simply
+                      random text. It has roots in a piece of classical Latin
+                      literature from 45 BC, making it over 2000 years old.
+                      Richard McClintock, a Latin professor at Hampden-Sydney
+                      College in Virginia.
+                    </Card.Text>
+                    <div className='d-grid d-md-block'>
+                      <Card.Link as={Button} variant='light'>
+                        Call-to-action
+                      </Card.Link>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col md={{ span: 6, offset: 2 }} className='d-none d-md-block'>
+                <Image alt='person watching screen' src={block4pic} />
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </LandingPageLayout>
-      <footer></footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
